240c891e70585f8f73cb111f330cc6752d2c664a
Chun-Hung <32384566+r06942072@users.noreply.github.com>
2019-04-10 11:53:47 -0400

    add Table to WIX

--- code_Youtuber/pagecode.js
+++
@@ -1,6 +0,0 @@
-//pass information as the form array of json object to IFrame
-
-export function videoES_viewportEnter(event) {
-	let isRendered = $w("#videoES").rendered;
-	$w("#textES").text = "I love you";
-}
--- code_cuisine/pagecode.js
+++ code_cuisine/pagecode.js
@@ -1,29 +1,29 @@
-//pass information as the form array of json object to IFrame
-import { get_44 } from 'backend/get_query.jsw';
+//cuisine
+import { get_collection } from 'backend/get_query.jsw';
+let collection_id = "my44_10datas";
 
 $w.onReady(function () {
-	let dropdown_value = $w("#dropdown1").value;
-	update_page(dropdown_value);
+	let dropdown_value = $w("#dropdown1").value;	
+	update_page(dropdown_value, collection_id);
 });
-
-
 export function dropdown1_change(event) {
 	let dropdown_value = $w("#dropdown1").value;
-	update_page(dropdown_value);
+	update_page(dropdown_value, collection_id);
 }
-
-export function update_page(dropdown_value) {
-	get_44().then((obj) => {
+export function update_page(dropdown_value, collection_id) {
+	get_collection(collection_id).then((obj) => {
 		let obj_size = obj["items"].length;
-		let Arr = [];	
 		let style = obj["items"].map((item) => { return item["style"] });
 		let name = obj["items"].map((item) => { return item["name"] });
 		let url = obj["items"].map((item) => { return item["url"] });
 		let address = obj["items"].map((item) => { return item["address"] });
-		//push to html
+		//Organize as array of Object
+		let Arr = [];
 		for (let i = 0; i < obj_size; i++) {
 			if (style[i] === dropdown_value) {
+				let index_str = i.toString();
 				let data = {
+					index: index_str
 					style: style[i],
 					name: name[i],
 					url: url[i],
@@ -32,7 +32,29 @@ export function update_page(dropdown_value) {
 				Arr.push(data);
 			}
 		}
+		//push to html
 		$w('#html1').postMessage({ Arr });
+		//push to table
+		/*
+		let tableRows = [
+			{
+				"index": "John",
+				"chinese": "Doe",
+				"name": "john.doe@somedomain.com",
+				"link": "http://someImageUrl/john.jpg",
+				"address": "幹靠背"
+			},
+			{
+				"index": "Tony",
+				"chinese": "Doe",
+				"name": "john.doe@somedomain.com",
+				"link": "http://someImageUrl/john.jpg",
+				"address": "dd"
+			}
+		] */
+		//$w("#myTable").rows = Arr;
+
+		/*
 		//push to text
 		let final_text = '<Detail lists below>' + '\n' + 'Restaurant name, website' + '\n\n';
 		let order = 0;
@@ -45,5 +67,6 @@ export function update_page(dropdown_value) {
 			}
 		}
 		$w("#textTPE").text = final_text;
+		*/
 	});
 }
